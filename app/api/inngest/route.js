235816e5e190a52253a1c0cf683705f98d87d908
preciousekele <preshekele@gmail.com>
2025-08-09 01:32:26 +0100

    route

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,6 +1,5 @@
 import { serve } from "inngest/next";
 import { inngest, syncUserCreation, syncUserDeletion } from "@/config/inngest";
-import { helloWorld } from "../../../inngest/functions";
 
 export const { GET, POST, PUT } = serve({
   client: inngest,
