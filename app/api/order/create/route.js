e0dae5074e2ec3a08c49f9a01f45580863b1b7ac
preciousekele <preshekele@gmail.com>
2025-08-14 04:04:32 +0100

    imported order in inngest

--- app/api/order/create/route.js
+++ app/api/order/create/route.js
@@ -5,8 +5,6 @@ import { getAuth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
 
-
-
 export async function POST(request){
     try {
         
@@ -20,7 +18,7 @@ export async function POST(request){
         //calculate amount
         const amount = await items.reduce(async (acc, item) => {
             const product = await Product.findById(item.product);
-            return acc + product.offerPrice * item.quantity;
+            return await acc + product.offerPrice * item.quantity;
         },0)
 
         await inngest.send({
