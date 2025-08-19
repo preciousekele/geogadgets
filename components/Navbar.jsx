ea3b4ed7b6c0143a773f1344296f759520f4c70a
preciousekele <preshekele@gmail.com>
2025-08-20 00:09:02 +0100

    updated uis and apis

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -23,16 +23,16 @@ const Navbar = () => {
   <span className="text-yellow-700">G</span>EO GADGETS
 </div>
       <div className="flex items-center gap-4 lg:gap-8 max-md:hidden">
-        <Link href="/" className="hover:text-gray-900 transition">
+        <Link href="/" className="hover:text-gray-900 transition text-sm">
           Home
         </Link>
-        <Link href="/all-products" className="hover:text-gray-900 transition">
+        <Link href="/all-products" className="hover:text-gray-900 transition text-sm">
           Shop
         </Link>
         {/* <Link href="/" className="hover:text-gray-900 transition">
           About Us
         </Link> */}
-        <Link href="/" className="hover:text-gray-900 transition">
+        <Link href="/" className="hover:text-gray-900 transition text-sm">
           Contact
         </Link>
 
