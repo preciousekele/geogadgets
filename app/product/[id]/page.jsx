2a55011c3d70b62b5cf5f2cf72ab917ccae535d3
preciousekele <preshekele@gmail.com>
2025-08-20 22:38:47 +0100

    featured titles mb

--- app/product/[id]/page.jsx
+++ app/product/[id]/page.jsx
@@ -130,7 +130,7 @@ const Product = () => {
                 <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6 mt-6 pb-14 w-full">
                     {products.slice(0, 5).map((product, index) => <ProductCard key={index} product={product} />)}
                 </div>
-                <button onClick={() => { router.push('/all-products') }} className="px-12 py-2.5 border rounded text-gray-500/70 hover:bg-slate-50/90 transition">
+                <button onClick={() => { router.push('/all-products') }} className="px-12 py-2.5 border rounded text-gray-500/70 hover:bg-slate-50/90 transition mb-6">
                     See more
                 </button>
             </div>
